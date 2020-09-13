d07c8b026c3a35d4edccdd899411f7446d605a0c
Frank Mathy <frankmathy@gmail.com>
2020-09-13 12:14:06 +0200

    Added cards

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,54 @@
 import React from 'react';
 import './Home.css';
 import Banner from './Banner';
+import Card from './Card';
 
 function Home() {
   return (
     <div className="home">
       <Banner />
+
+      <div className="home__section">
+        <Card
+          src="https://a0.muscache.com/im/pictures/fdb46962-10c1-45fc-a228-d0b055411448.jpg?im_w=720"
+          title="Entire homes"
+          description="Comforable private places, with room for friends or family"
+          price="99.98"
+        />
+        <Card
+          src="https://a0.muscache.com/im/pictures/fdb46962-10c1-45fc-a228-d0b055411448.jpg?im_w=720"
+          title="Entire homes"
+          description="Comforable private places, with room for friends or family"
+          price="99.98"
+        />
+        <Card
+          src="https://a0.muscache.com/im/pictures/fdb46962-10c1-45fc-a228-d0b055411448.jpg?im_w=720"
+          title="Entire homes"
+          description="Comforable private places, with room for friends or family"
+          price="99.98"
+        />
+      </div>
+
+      <div className="home__section">
+        <Card
+          src="https://a0.muscache.com/im/pictures/fdb46962-10c1-45fc-a228-d0b055411448.jpg?im_w=720"
+          title="Entire homes"
+          description="Comforable private places, with room for friends or family"
+          price="99.98"
+        />
+        <Card
+          src="https://a0.muscache.com/im/pictures/fdb46962-10c1-45fc-a228-d0b055411448.jpg?im_w=720"
+          title="Entire homes"
+          description="Comforable private places, with room for friends or family"
+          price="99.98"
+        />
+        <Card
+          src="https://a0.muscache.com/im/pictures/fdb46962-10c1-45fc-a228-d0b055411448.jpg?im_w=720"
+          title="Entire homes"
+          description="Comforable private places, with room for friends or family"
+          price="99.98"
+        />
+      </div>
     </div>
   );
 }
